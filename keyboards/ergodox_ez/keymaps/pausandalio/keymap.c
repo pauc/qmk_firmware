1effbaee130cb49d04aa5a1c7085ccfd80c2d57f
Pau Compte <pausandalio@gmail.com>
2019-02-07 17:19:47 +0100

    v1.0.2

--- keyboards/ergodox_ez/keymaps/pausandalio/keymap.c
+++ keyboards/ergodox_ez/keymaps/pausandalio/keymap.c
@@ -2,8 +2,6 @@
 #include "debug.h"
 #include "action_layer.h"
 #include "version.h"
-#include "keymap_german.h"
-#include "keymap_nordic.h"
 
 #define BASE  0 // default layer
 #define SYMB  1 // symbols & numbers
@@ -119,11 +117,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Symbols and numbers
  *
  * ,---------------------------------------------------------.          ,---------------------------------------------------------.
- * |    ^    |   \   |   /   |   |   |   =   |   +   |   *   |          |       |       |   ,   |   0   |   .   |       |         |
+ * |    ^    |   \   |   /   |   |   |   =   |   +   |   *   |          |       |       |       |   /   |   *   |   -   |         |
  * |---------+-------+-------+-------+-------+-------+-------|          |-------+-------+-------+-------+-------+-------+---------|
- * |    ¿    |   ?   |   ;   |   &   |   -   |   "   |       |          |       |       |   7   |   8   |   9   |       |         |
+ * |    ¿    |   ?   |   ;   |   &   |   -   |   "   |       |          |       |       |   7   |   8   |   9   |   +   |         |
  * |---------+-------+-------+-------+-------+-------|       |          |       |-------+-------+-------+-------+-------+---------|
- * |    ¡    |   !   |   @   |  (/)  |   _   |   '   |-------|          |-------|       |   4   |   5   |   6   |       |         |
+ * |    ¡    |   !   |   @   |  (/)  |   _   |   '   |-------|          |-------|       |   4   |   5   |   6   |   =   |         |
  * |---------+-------+-------+-------+-------+-------|       |          |       |-------+-------+-------+-------+-------+---------|
  * |         |   ~   |   #   |   $   |   %   |   `   |       |          |       |       |   1   |   2   |   3   |       |         |
  * `---------+-------+-------+-------+-------+---------------'          `---------------+-------+-------+-------+-------+---------'
@@ -149,11 +147,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                       KC_TRANSPARENT, KC_TRANSPARENT, TO(0),
 
                                                                                                  // RIGHT HAND -------------------------------------------------------------------------------------------------
-                                                                                                                      KC_TRANSPARENT, KC_TRANSPARENT, KC_COMMA, KC_0, KC_KP_DOT, KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                                      KC_TRANSPARENT, KC_TRANSPARENT, KC_7,     KC_8, KC_9,      KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                                                      KC_TRANSPARENT, KC_4,     KC_5, KC_6,      KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                                      KC_TRANSPARENT, KC_TRANSPARENT, KC_1,     KC_2, KC_3,      KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                                                                      KC_COMMA, KC_0, KC_KP_DOT, KC_TRANSPARENT, KC_TRANSPARENT,
+                                                                                                                      KC_NO, KC_NO, KC_NO,    KC_KP_SLASH, KC_KP_ASTERISK, KC_MINUS, KC_TRANSPARENT,
+                                                                                                                      KC_NO, KC_NO, KC_7,     KC_8,        KC_9,           KC_PLUS,  KC_NO,
+                                                                                                                             KC_NO, KC_4,     KC_5,        KC_6,           KC_EQUAL, KC_NO,
+                                                                                                                      KC_NO, KC_NO, KC_1,     KC_2,        KC_3,           KC_NO,    KC_NO,
+                                                                                                                                    KC_COMMA, KC_0,        KC_KP_DOT,      KC_NO,    KC_NO,
                                                                                                   KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                   KC_TRANSPARENT,
                                                                                                   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT),

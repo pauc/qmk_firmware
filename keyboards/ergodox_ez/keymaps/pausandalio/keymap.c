bb981e8ee6d6dfb6162557a2065f92dbfa41ebb1
Pau Compte <pausandalio@gmail.com>
2020-08-31 21:38:25 +0200

    v2.0.1

--- keyboards/ergodox_ez/keymaps/pausandalio/keymap.c
+++ keyboards/ergodox_ez/keymaps/pausandalio/keymap.c
@@ -390,7 +390,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void led_set_user(uint8_t usb_led) {
-  if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+  if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
     ergodox_right_led_1_on();
     ergodox_right_led_2_on();
     ergodox_right_led_3_on();
